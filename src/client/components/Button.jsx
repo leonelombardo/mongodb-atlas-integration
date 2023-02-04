3e937cdb4be5604ae95a51e872d18d8f1c1d7ad4
Leonel <leonelombardo1@gmail.com>
2023-02-04 17:20:29 -0300

    components: new props for button (type=button, size=lg, onClick) + motion and variants added to layout

--- src/client/components/Button.jsx
+++ src/client/components/Button.jsx
@@ -1,3 +1,7 @@
-export const Button = ({ children }) => {
-    return <button className="py-2 px-4 bg-mongodb-500 rounded-full text-lg text-secondary-500 font-bold min-w-[150px] w-full hover:brightness-125 transition-[filter] duration-300">{ children }</button>
+export const Button = ({ type="button", size="lg", onClick, children }) => {
+    return <>
+        {
+         children && <button type={type} onClick={onClick} className={`py-2 px-4 bg-mongodb-500 rounded-full text-${size} text-secondary-500 font-${size !== "lg" ? "normal" : "bold"} min-w-[150px] w-full h-fit hover:brightness-125 transition-[filter] duration-300`}>{ children }</button>
+        }
+    </>
 }
--- src/client/components/Layout.jsx
+++ src/client/components/Layout.jsx
@@ -1,3 +1,17 @@
+import { motion } from "framer-motion"
+
 export const Layout = ({ children }) => {
-    return <div className="flex flex-col justify-center items-center min-w-screen w-full min-h-screen h-screen gap-1" >{ children }</div>
+    const layoutVariants = {
+        initial: {
+            opacity: 0,
+        },
+        animate: {
+            opacity: 1
+        },
+        exit: {
+            opacity: 0
+        }
+    }
+    
+    return <motion.div {...layoutVariants} className="flex flex-col justify-center items-center min-w-screen w-full min-h-screen h-screen gap-1" >{ children }</motion.div>
 }
