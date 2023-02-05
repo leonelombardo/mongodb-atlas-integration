cf12a591e1fbcb2443166b2ce027024db115c526
Leonel <leonelombardo1@gmail.com>
2023-02-05 10:46:46 -0300

    update: detecting theme color <App/> + html tag styles for color theme

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,8 +1,16 @@
+import { useEffect } from "react"
 import { BrowserRouter as Router } from "react-router-dom"
 
 import { AnimatedRoutes } from "./AnimatedRoutes"
 
 export const App = () => {
+  useEffect(() => {
+    const themeColor = window.localStorage.getItem("theme-color")
+
+    if(themeColor === "dark") document.documentElement.classList.add("dark")
+    else document.documentElement.classList.remove("dark")
+  }, [])
+
   return (
     <>
       <Router>
